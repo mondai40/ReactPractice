937c269cf98bd69d557f4dc744da89984153f36d
40 daimon <tvwmdaimon40@gmail.com>
2019-11-13 23:38:03 -0800

    add hellow world

--- selfStudy2/my-app/src/index.js
+++ selfStudy2/my-app/src/index.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const lakeList = [
-    {id: 1, name: "Echo Lake", trailhead: "Echo Lake"},
-    {id: 2, name: "Maud Lake", trailhead: "Wright's Lake"},
-    {id: 3, name: "Cascade Lake", trailhead: "Bayview"}
-];
+const style = {
+    backgroundColor: "orange",
+    color: "white",
+    fontFamily: "Arial"
+};
 
-ReactDOM.render(
-    <App lakes={lakeList} />,
-    document.getElementById("root")
+
+const title  = React.createElement(
+    "h1",
+    {id: "title", className: "header", style: style},
+    "Hello World"
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    title,
+    document.getElementById("root")
+);
