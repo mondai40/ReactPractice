2a6283df1330f957d7bc7ca87d3a2c7b1b59c0e3
40 daimon <tvwmdaimon40@gmail.com>
2019-11-18 21:54:42 -0800

    add setState

--- selfStudy2/my-app/src/index.js
+++ selfStudy2/my-app/src/index.js
@@ -1,47 +1,52 @@
 import React, {Component} from "react";
 import {render} from "react-dom";
 
-let skiData = {
-    total: 50,
-    powder: 20,
-    backCountry: 10,
-    goal: 100,
-};
+const bookList = [
+    {"title": "Hunger", "author": "Roxane Gay", "pages": 320},
+    {"title": "The Sun Also Rise", "author": "Ernest Hemingway", "pages": 260},
+    {"title": "White Teeth", "author": "Zadid Smith", "pages": 480},
+    {"title": "Cat's Cradle", "author": "Kurt Vonnegut", "pages": 304},
+];
 
-class SkiDay extends Component {
-    getPercent = (decimal) => {
-        return  decimal * 100 + "%";
+const Book = ({title, author, pages}) => {
+    return(
+        <section>
+            <h2>{title}</h2>
+            <p>By: {author}</p>
+            <p>Pages: {pages} pages</p>
+        </section>
+    );
+}
+
+class Library extends React.Component {
+    state = {open: true};
+    
+    toggleOpenClosed = () => {
+        this.setState(prevState => ({
+            open: !prevState.open,
+        }));
     }
-    clacGoalProgress = (total, goal) => {
-        return this.getPercent(total/goal);
-    }
+
     render() {
-        const {total, powder, backCountry, goal} = this.props;
+        const {books} = this.props;
         return(
-            <section>
-                <div>
-                    <p>Total Days: {total}</p>
-                </div>
-                <div>
-                    <p>Powder Days: {powder}</p>
-                </div>
-                <div>
-                    <p>Backcountry Days: {backCountry}</p>
-                </div>
-                <div>
-                    <p>Goal Progress : {this.clacGoalProgress(total, goal)}</p>
-                </div>
-            </section>
+            <div>
+                <h1>The library is {this.state.open? "open": "closed"}.</h1>
+                <button onClick={this.toggleOpenClosed}>Click</button>
+                {books.map((book, i) => 
+                    <Book
+                        key={i}
+                        title={book.title} 
+                        author={book.author} 
+                        pages={book.pages}
+                    />
+                )}
+            </div>
         );
     }
 }
 
 render(
-    <SkiDay 
-        total={skiData.total}
-        powder={skiData.powder}
-        backCountry={skiData.backCountry}
-        goal={skiData.goal}
-    />,
+    <Library books={bookList}/>,
     document.getElementById("root")
 );
